c887d22ceefd8b705c765d4cd06ad476130dbbad
WORLDSKILLS <juandiego1918@gmail.com>
2018-08-28 15:23:15 -0500

    try

--- app/src/main/java/com/example/worldskills/myapplication/MainActivity.java
+++ app/src/main/java/com/example/worldskills/myapplication/MainActivity.java
@@ -4,21 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Chronometer;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Chronometer crono;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        crono = findViewById(R.id.crono);
+        crono.start();
     }
 
 
     public void onClick(View view) {
-
-        Intent miIntent=new Intent(MainActivity.this,MapsActivity.class);
+        Toast.makeText(getApplicationContext(), "" + crono.getContentDescription(), Toast.LENGTH_SHORT).show();
+        Intent miIntent = new Intent(MainActivity.this, MapsActivity.class);
         startActivity(miIntent);
     }
 }
